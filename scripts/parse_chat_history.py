cd56c253831f2d0d1d4e9608397e1fa416e21be7
Matheus Miranda <matheusmirandalacerda@gmail.com>
2018-08-23 09:41:22 -0300

    Add methods to get messages from a Direct Channel
    Signed-off-by: Gabriela Barrozo Guedes <gabrielabguedes@gmail.com>
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

---
+++ scripts/parse_chat_history.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+import argparse
+import json
+import logging
+import requests
+
+# == Log Config ==
+
+logger = logging.getLogger('Parse Chats History')
+logger.setLevel(logging.DEBUG)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter(
+    '%(asctime)s :: %(name)s :: %(levelname)s :: %(message)s'
+)
+
+ch.setFormatter(formatter)
+
+logger.addHandler(ch)
+
+# == CLI ==
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--user-name', '-un', type=str, default='rouana',
+    help='Admin username (default: rouana)'
+)
+parser.add_argument(
+    '--user-password', '-up', type=str, default='rouana',
+    help='Admin password (default: rouana)'
+)
+parser.add_argument(
+    '--rocketchat-url', '-r', type=str, default='http://localhost:3000',
+    help='Rocket chat URL (default: http://localhost:3000)'
+)
+
+args = parser.parse_args()
+
+
+host = args.rocketchat_url
+if host[-1] == '/':
+    host = host[:-1]
+
+login_path = '/api/v1/login'
+
+user_name = args.user_name
+user_password = args.user_password
+user_header = None
+
+def get_authentication_token():
+    login_data = {'username': user_name, 'password': user_password}
+    response = requests.post(host + login_path, data=json.dumps(login_data))
+
+    if response.json()['status'] == 'success':
+        logger.info('Login suceeded')
+
+        authToken = response.json()['data']['authToken']
+        userId = response.json()['data']['userId']
+        user_header = {
+            'X-Auth-Token': authToken,
+            'X-User-Id': userId,
+            'Content-Type': 'application/json'
+        }
+
+        return user_header
+
+def get_user_rooms():
+    get_rooms_response = requests.get(
+        host + '/api/v1/rooms.get',
+        headers = user_header
+    )
+
+    channels_ids = []
+    rooms_data = get_rooms_response.json()['update']
+
+    for channel_data in rooms_data:
+        channels_ids.append(channel_data['_id']) 
+
+    return channels_ids
+
+def get_channel_history(channel_id):
+    get_history_response = requests.get(
+        host + '/api/v1/im.history?roomId=6wt5yx4rw72RP6ArzBQfg2knuGcaMFpHsd',
+        headers = user_header
+    )
+
+    messages_data = get_history_response.json()['messages']
+
+    for message_data in messages_data:
+        user_name = messages_data['username']
+        message = messages_data['msg']
+
+if __name__ == '__main__':
+    logger.info('===== Automatic env configuration =====')
+
+    user_header = get_authentication_token()
+
+    if user_header:
+        logger.info('>> Get all rooms for user {}'.format(user_name))
+        channels_ids = get_user_rooms()
+    
+        for channel_id in channels_ids:     
+            logger.info('>> Get messages for channel {}'.format(channel_id))
+            get_channel_history(channel_id)
+    
+    else:
+        logger.error('Login Failed')

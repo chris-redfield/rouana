35d0494a7daaed7a3acacae84d0bc1318813255b
Matheus Miranda <matheusmirandalacerda@gmail.com>
2018-08-26 20:46:26 -0300

    Add REAL TIME connection in order to user 'loadHistory' call method
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- scripts/parse_chat_history.py
+++ scripts/parse_chat_history.py
@@ -5,6 +5,8 @@ import json
 import logging
 import requests
 import os
+import time
+from rocketchat import RocketChatBot
 
 # == Log Config ==
 
@@ -51,6 +53,7 @@ login_path = '/api/v1/login'
 user_name = args.user_name
 user_password = args.user_password
 user_header = None
+channels_ids = []
 
 def get_authentication_token():
     login_data = {'username': user_name, 'password': user_password}
@@ -96,47 +99,74 @@ def create_file(path, file_name):
     file = open(path + file_name, 'w+')
     return file
 
-def get_channel_history(channel_id):
-    get_history_response = requests.get(
-        host + '/api/v1/im.history?roomId=' + channel_id,
-        headers = user_header
-    )
-    if get_history_response.json()['success']:
-        messages_data = get_history_response.json()['messages']
-        path = 'messages/' + str(channel_id) + '/'
-        create_folder(path)
-        file_name = messages_data[0]['ts'][:10] + '.txt'
-        f = create_file(path, file_name)
+def create_bot():
+    bot = RocketChatBot(user_name,user_password,server='localhost:3000',ssl=False)
+    return bot
 
-        for message_data in reversed(messages_data):
-            time = message_data['ts']
+def process_channel_messages(conversation_id, conversation_messages):
+    #for el in conversation_messages:
+        #print("foi \n\n", str(el['ts'])[:10],"\n\n")
+        #if 'name' in el['u']:
+        #    user_name = el['u']['name']
+        #else:
+        #    user_name = el['u']['username']
+    #    print(str(el['ts'])[:10])
+
+    path = 'messages/' + str(conversation_id) + '/'
+    create_folder(path)
+    file_name = str(conversation_messages[0]['ts'])[:10] + '.txt'
+    f = create_file(path, file_name)
+
+    for message_data in reversed(conversation_messages):
+        time = str(message_data['ts'])
+        username = ""
+
+        if 'name' in message_data['u'] and message_data['u']['name'] != None:
+            username = message_data['u']['name']
+        else:
             username = message_data['u']['username']
-            message = message_data['msg']
-            line = time + ' ' + username +': '+ message + '\n'
 
-            if time[:10] != file_name[:10]:
-                f.close()
-                f = create_file(path, time[:10] + '.txt')
+        if username == None:
+            print("Message data foi \n", message_data, "\n\n")
 
-            f.write(line)
+        message = message_data['msg']
 
-        f.close()
-    else:
-        logger.error('>> Cannot get the messages of this channel')
+        line = time + ' ' + username +': '+ message + '\n'
+
+        if time[:10] != file_name[:10]:
+            print("Entrou aki e time foi ",time[:10]," e file_name foi ",file_name[:10])
+            f.close()
+            f = create_file(path, time[:10] + '.txt')
+            print("ENTROUUUU AKIII e line foi", line,"\n\n\n")
+
+        f.write(line)
+
+    f.close()
+
+def get_channels_history(bot, message):
+    for channel_id in channels_ids:
+        logger.info('>> Get messages for channel {}'.format(channel_id))
+        bot.getHistory(channel_id)
 
 if __name__ == '__main__':
     logger.info('===== Automatic env configuration =====')
 
     user_header = get_authentication_token()
+    bot = create_bot()
+    bot.addPrefixHandler('get messages', get_channels_history)
 
     if user_header:
         create_folder('messages')
         logger.info('>> Get all rooms for user {}'.format(user_name))
         channels_ids = get_user_rooms()
 
-        for channel_id in channels_ids:
-            logger.info('>> Get messages for channel {}'.format(channel_id))
-            get_channel_history(channel_id)
+        bot.start()        
 
+        while True:
+            if len(bot.conversations_messages) == len(channels_ids):
+                break
+
+        for conversation_id, conversation_messages in bot.conversations_messages.items():
+            process_channel_messages(conversation_id, conversation_messages)
     else:
         logger.error('Login Failed')
